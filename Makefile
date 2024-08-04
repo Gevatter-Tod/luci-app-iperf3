75ece49d13bd498daa2d20fe77b25c1ba0005b2c
Gevatter-Tod <nurshoppen@gmx.at>
2024-08-04 19:54:06 +0200

    v0.1 server working, client broken

---
+++ Makefile
@@ -0,0 +1,12 @@
+include $(TOPDIR)/rules.mk
+
+LUCI_TITLE:=LuCI support for iperf3
+LUCI_DEPENDS:=+iperf3
+LUCI_PKGARCH:=all
+PKG_VERSION:=1.0.0
+PKG_RELEASE:=1
+
+include ../../luci.mk
+
+# call BuildPackage - OpenWrt buildroot signature
+$(eval $(call BuildPackage,luci-app-iperf3))
---
+++ htdocs/luci-static/resources/view/iperf3/index.js
@@ -0,0 +1,95 @@
+'use strict';
+'require view';
+'require form';
+'require fs';
+'require ui';
+
+return view.extend({
+    load: function() {
+        return fs.exec_direct('/usr/bin/iperf3', ['--version']);
+    },
+
+    render: function(version) {
+        var m, s, o;
+
+        m = new form.Map('iperf3', _('iPerf3'), _('This is the iPerf3 configuration page.'));
+
+        s = m.section(form.TypedSection, 'iperf3', _('Settings'));
+        s.anonymous = true;
+
+        o = s.option(form.Value, 'server', _('Server'), _('The iPerf3 server to connect to.'));
+        o.datatype = 'host';
+
+        o = s.option(form.Value, 'port', _('Port'), _('The port to use for the iPerf3 connection.'));
+        o.datatype = 'port';
+        o.placeholder = '5201';
+
+        o = s.option(form.Flag, 'reverse', _('Reverse'), _('Run in reverse mode.'));
+        o.default = o.disabled;
+
+        // Add a button to start the iperf3 test
+        o = s.option(form.Button, '_start', _('Start Test'));
+        o.inputtitle = _('Start iPerf3 Test');
+        o.onclick = this.handleStartTest;
+
+        // Add a button to start the iperf3 server
+        o = s.option(form.Button, '_start_server', _('Start Server'));
+        o.inputtitle = _('Start iPerf3 Server');
+        o.onclick = this.handleStartServer;
+
+        // Add a button to stop the iperf3 server
+        o = s.option(form.Button, '_stop_server', _('Stop Server'));
+        o.inputtitle = _('Stop iPerf3 Server');
+        o.onclick = this.handleStopServer;
+
+        // Add a placeholder for the results
+        o = s.option(form.DummyValue, '_results', _('Results'));
+        o.textvalue = '<div id="iperf3-results"></div>';
+
+        return m.render();
+    },
+
+
+    // this function still has errors
+    handleStartTest: function() {
+        var server = document.querySelector('[data-name="server"]').value;
+        var port = document.querySelector('[data-name="port"]').value || '5201';
+        var reverse = document.querySelector('[data-name="reverse"]').checked ? '-R' : '';
+    
+        //var parameter = `-c ${server} -p ${port} ${reverse}`;
+        //var parameter = `-c 192.168.45.121 -p 5201`;
+
+        //befehl kurzgeschlossen
+        fs.exec('/usr/bin/iperf3', ['-c', '192.168.45.121', '-p', ' 5201']).then(function(res) {
+                // Check if res.stdout is defined
+                if (res.stdout && res.stdout.length > 0) {
+                    document.getElementById('iperf3-results').innerText = res.stdout.trim();
+                } else {
+                    // Handle case where stdout is undefined or empty
+                    ui.addNotification(null, _('Failed to start iPerf3 test: No output received'), 'error');
+                }
+            })
+            .catch(function(err) {
+                // Improved error handling
+                ui.addNotification(null, _('Failed to start iPerf3 test: ') + err.message, 'error');
+            });
+    },
+
+    handleStartServer: function() {
+        fs.exec('/usr/bin/iperf3', ['-s', '-D']).then(function(res) {
+            ui.addNotification(null, _('iPerf3 server started successfully'), 'info');
+        }).catch(function(err) {
+            ui.addNotification(null, _('Failed to start iPerf3 server: ') + err.message, 'error');
+        });
+    },
+
+    handleStopServer: function() {
+        var command = `killall iperf3`;
+
+        fs.exec_direct('killall', ['iperf3']).then(function(res) {
+            ui.addNotification(null, _('iPerf3 server stopped successfully'), 'info');
+        }).catch(function(err) {
+            ui.addNotification(null, _('Failed to stop iPerf3 server: ') + err.message, 'error');
+        });
+    }
+});
